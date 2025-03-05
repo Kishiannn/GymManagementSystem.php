053699ee9eda0032536133a3945c084d66556283
Kishiannn_ <abudachan122@gmail.com>
2025-03-05 23:26:02 +0800

    Update and rename index.php to customer_homepage.php

--- customer_homepage.php
+++ customer_homepage.php
@@ -10,10 +10,11 @@ require "conn.php" ;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<?php include 'navbar.php'?>
+<?php include 'customer_navbar.php'?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
